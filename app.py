4a3f2c4465d0ccd2d796ccd02ae920cd82dd3c0a
Aryanrai2005 <129605931+Aryanrai2005@users.noreply.github.com>
2024-01-24 10:06:51 +0000

    Pending changes exported from your codespace

--- app.py
+++ app.py
@@ -5,3 +5,12 @@ app = Flask(__name__)
 @app.route("/")
 def hello_world():
     return render_template("index.html", title="Hello")
+@app.route("/hello")
+def hello():
+    return "Hello World"
+@app.route("/name")
+def name():
+    return "Aryan"
+@app.route("/age")
+def age():
+    return 18
